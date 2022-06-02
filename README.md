dcbe5921c363aeb119a88b061001393d677add11
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2022-06-02 13:15:28 +0200

    Corrected the equation in the code

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Basically what the title sounds like, trying to get an optimized fit and then us
 In order to create
 
 We get a local maximum with p0 = [0.8, 1, 5.48, 3.69047095e+13, 1e20]
+
+We can write the differential equation in the form 
+![img](http://www.sciweavers.org/upload/Tex2Img_1654167900/render.png)
--- main.py
+++ main.py
@@ -11,12 +11,12 @@ ydata = ydata * np.sqrt(2)
 
 def func(x, a, b, c, d, e):
     return 1e10 * np.sqrt((M * x ** 2 * V0 * (d * x - b * x ** 3)) ** 2 / (
-            (e - c * x ** 2 + a * x ** 4 - M * x ** 4) ** 2 + (d * x - b * x ** 3) ** 2) ** 2 + (
-                                  M * x ** 2 * V0 * (e - c * x ** 2 + a * x ** 4 - M * x ** 4)) ** 2 / (
-                                  (e - c * x ** 2 + a * x ** 4 - M * x ** 4) ** 2 - (d * x - b * x ** 3) ** 2) ** 2)
+            (e - c * x ** 2 + a * x ** 4 - M ** 2 * x ** 4) ** 2 + (d * x - b * x ** 3) ** 2) ** 2 + (
+                                  M * x ** 2 * V0 * (e - c * x ** 2 + a * x ** 4 - M ** 2 * x ** 4)) ** 2 / (
+                                  (e - c * x ** 2 + a * x ** 4 - M ** 2 * x ** 4) ** 2 - (d * x - b * x ** 3) ** 2) ** 2)
 
 
-popt, pcov = curve_fit(func, xdata, ydata, p0=[0.8, 1, 5.48, 3.69047095e+13, 1e20], maxfev=500000)
+popt, pcov = curve_fit(func, xdata, ydata, p0=[0.01, 0.04, 2e10, 1.400001e14, 1e20], maxfev=500000)
 print(M)
 print(popt)
 # print(pcov)
