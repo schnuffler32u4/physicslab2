b9744e82e2c11988179107f849f6f68edeb17e3c
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2022-06-04 00:37:59 +0200

    Fixed RLC value extractor

--- functions.py
+++ functions.py
@@ -20,11 +20,11 @@ def circuit_values(a, b, c, d, e):
     C1 = 1 / (e * C2)
 
     def leq(x):
-        return a / (x * C2) + C1 * (b - a * C1 * d) / (x - a * C1 / (x * C2)) * (
-                d - (b - a * C1 * d) / (C2 * (x - a * C1 / (x * C2)))) + x / C1 - c
+        return a / (x * C2) + C1 * (b - a * C1 * d /x ) / (x - a * C1 / (x * C2)) * (
+                d - (b - a * C1 * d / x) / (C2 * (x - a * C1 / (x * C2)))) + x / C1 - c
 
     L2 = fsolve(leq, 1, maxfev=500000)[0]
     L1 = a / L2
-    R1 = (b - a * C1 * d) / (L2 - a * C1 / (L2 * C2))
+    R1 = (b - a * C1 * d / L2) / (L2 - a * C1 / (L2 * C2))
     R2 = C1 * (d - R1 / C2)
     return [L1, L2, R1, R2, C1]
