1e8b89b1a3b9481aaf3c3fec2e0bcd6f05296eca
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2022-06-03 18:41:54 +0200

    Better values, fixed more math

--- functions.py
+++ functions.py
@@ -30,13 +30,13 @@ def circuit_values(a, b, c, d, e):
     """The function takes as input the values of a,b,c,d,e and returns the corresponding capacitances, resistances
     and so on. We assume C2 = 1e-10. The output is an array of the form [L1,L2,R1,R2,C1]"""
     C2 = 1e-10
-    C1 = e / C2
+    C1 = 1 / (e * C2)
 
     def leq(x):
-        return a / (x * C2) + (b - a * C1 * d) / (x - a * C1 / (x * C2)) * (
+        return a / (x * C2) + C1 * (b - a * C1 * d) / (x - a * C1 / (x * C2)) * (
                     d - (b - a * C1 * d) / (C2 * (x - a * C1 / (x * C2)))) + x / C1 - c
 
-    L2 = fsolve(leq, 0.0879, maxfev=500000)[0]
+    L2 = fsolve(leq, 1, maxfev=500000)[0]
     L1 = a / L2
     R1 = (b - a * C1 * d) / (L2 - a * C1 / (L2 * C2))
     R2 = C1 * (d - R1 / C2)
