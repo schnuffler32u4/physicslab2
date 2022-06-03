c4ee93b97c8f5584a2374bffd744009b4324e279
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2022-06-03 11:13:09 +0200

    Fixed the factors and a first try of extracting values

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import scipy.integrate as si
 import numpy as np
+from scipy.optimize import fsolve
 
 
 def midpoint_double(f, a, b, c, d, nx, ny):
@@ -21,7 +22,22 @@ dis = 0.05
 def f(x, y):
     return 200 ** 2 * 10 ** (-7) * R ** 2 * np.cos(x - y) / (np.sqrt(2 * R ** 2 * (1 - np.cos(x - y)) + dis ** 2))
 
-M = si.dblquad(f, 0, 2*np.pi, 0, 2*np.pi)[0]
+
+M = si.dblquad(f, 0, 2 * np.pi, 0, 2 * np.pi)[0]
 
 
+def circuit_values(a, b, c, d, e):
+    """The function takes as input the values of a,b,c,d,e and returns the corresponding capacitances, resistances
+    and so on. We assume C2 = 1e-10. The output is an array of the form [L1,L2,R1,R2,C1]"""
+    C2 = 1e-10
+    C1 = e / C2
 
+    def leq(x):
+        return a / (x * C2) + (b - a * C1 * d) / (x - a * C1 / (x * C2)) * (
+                    d - (b - a * C1 * d) / (C2 * (x - a * C1 / (x * C2)))) + x / C1 - c
+
+    L2 = fsolve(leq, 0.0879, maxfev=500000)[0]
+    L1 = a / L2
+    R1 = (b - a * C1 * d) / (L2 - a * C1 / (L2 * C2))
+    R2 = C1 * (d - R1 / C2)
+    return [L1, L2, R1, R2, C1]
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import numpy as np
 from functions import M
 from scipy.optimize import curve_fit, least_squares
 import matplotlib.pyplot as plt
+import functions
 
 V0 = 10.3
 xdata, ydata = np.loadtxt("fittry.csv", delimiter=",", skiprows=1, unpack=True, encoding='UTF8')
 xdata = 1000 * xdata * 2 * np.pi
 ydata = ydata * np.sqrt(2)
-M = 0.09
+
+
+# M = 0.06
 
 
 def func(x, a, b, c, d, e):
@@ -19,13 +22,23 @@ def func(x, a, b, c, d, e):
     return 1e10 * np.sqrt(A ** 2 + B ** 2)
 
 
-popt, pcov = curve_fit(func, xdata, ydata, p0=[0.01, 1.4, 2e9, 1.4e14, 1e20],  maxfev=500000)
+def funcvar(x, L1, L2, R1, R2, C1):
+    a = L1 * L2
+    b = L1 * R2 + R1 * L2
+    c = L1 / 1e-10 + R1 * R2 + L2 / C1
+    d = R1 / 1e-10 + R2 / C1
+    e = 1 / (C1 * 1e-10)
+    return func(x, a, b, c, d, e)
+
+
+popt, pcov = curve_fit(func, xdata, ydata, p0=[0.01, 1.4, 2e9, 1.4e14, 1e20], maxfev=500000)
 print(M)
-# x = np.linspace(0, 900000, 10000)
+
 print(popt)
-# print(pcov)
+print(functions.circuit_values(*popt))
+
 plt.plot(xdata, func(xdata, *popt), label='fit')
 plt.plot(xdata, ydata)
-# plt.plot(x, func(x, 0.01, 1.4, 2e9, 1.4e11, 1e20))
+plt.plot(xdata, funcvar(xdata, *functions.circuit_values(*popt)), label='yummy')
 plt.legend()
 plt.show()
