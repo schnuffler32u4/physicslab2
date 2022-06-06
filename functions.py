175770a1aba7232f134a9da129fd67f5dfeb3e56
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2022-06-07 01:36:49 +0200

    Moved the functions and tried to fit voltage vs distance

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import scipy.integrate as si
 import numpy as np
 from scipy.optimize import fsolve
 
+V0 = 10.3
+
 
 def mutual(dis):
     """The function takes the distance in meters as an input and outputs the mutual inductance between the coils."""
@@ -13,6 +15,50 @@ def mutual(dis):
     return si.dblquad(f, 0, 2 * np.pi, 0, 2 * np.pi)[0]
 
 
+M = mutual(0.05)
+
+
+def volt(x, a, b, c, d, e):
+    """Returns the voltage for the given a,b,c,d,e for a value of omega (x), for the M previously defined"""
+    s = a * x ** 4 - M ** 2 * x ** 4 - c * x ** 2 + e
+    t = d * x - b * x ** 3
+    p = M * x ** 2 * V0
+    A = p * t / (s ** 2 + t ** 2)
+    B = p * s / (s ** 2 + t ** 2)
+    return 1e10 * np.sqrt(A ** 2 + B ** 2)
+
+
+def voltvar(x, L1, L2, R1, R2, C1):
+    """Returns the value of the voltage for given circuit values and omega (x), assuming C2 = 1e-10"""
+    a = L1 * L2
+    b = L1 * R2 + R1 * L2
+    c = L1 / 1e-10 + R1 * R2 + L2 / C1
+    d = R1 / 1e-10 + R2 / C1
+    e = 1 / (C1 * 1e-10)
+    return volt(x, a, b, c, d, e)
+
+
+def letters(L1, L2, R1, R2, C1):
+    """Returns the a,b,c,d,e for the given values of the circuits"""
+    a = L1 * L2
+    b = L1 * R2 + R1 * L2
+    c = L1 / 1e-10 + R1 * R2 + L2 / C1
+    d = R1 / 1e-10 + R2 / C1
+    e = 1 / (C1 * 1e-10)
+    return [a, b, c, d, e]
+
+
+def voltdis(x, a, b, c, d, e, M):
+    """Returns the voltage for an omega (x) for a,b,c,d,e variables and a given M (which corresponds to a specific
+    distance """
+    s = a * x ** 4 - M ** 2 * x ** 4 - c * x ** 2 + e
+    t = d * x - b * x ** 3
+    p = M * x ** 2 * V0
+    A = p * t / (s ** 2 + t ** 2)
+    B = p * s / (s ** 2 + t ** 2)
+    return 1e10 * np.sqrt(A ** 2 + B ** 2)
+
+
 def circuit_values(a, b, c, d, e):
     """The function takes as input the values of a,b,c,d,e and returns the corresponding capacitances, resistances
     and so on. We assume C2 = 1e-10. The output is an array of the form [L1,L2,R1,R2,C1]"""
@@ -20,7 +66,7 @@ def circuit_values(a, b, c, d, e):
     C1 = 1 / (e * C2)
 
     def leq(x):
-        return a / (x * C2) + C1 * (b - a * C1 * d /x ) / (x - a * C1 / (x * C2)) * (
+        return a / (x * C2) + C1 * (b - a * C1 * d / x) / (x - a * C1 / (x * C2)) * (
                 d - (b - a * C1 * d / x) / (C2 * (x - a * C1 / (x * C2)))) + x / C1 - c
 
     L2 = fsolve(leq, 1, maxfev=500000)[0]
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scipy.optimize import curve_fit, least_squares
 import matplotlib.pyplot as plt
 import functions
 from matplotlib.widgets import Slider
+# plt.style.use('extensys')
 
 V0 = 10.3
 xdata, ydata = np.loadtxt("fittry.csv", delimiter=",", skiprows=1, unpack=True, encoding='UTF8')
@@ -11,74 +12,56 @@ ydata = ydata * np.sqrt(2)
 newx, newy = np.loadtxt("fitcheck.csv", delimiter=",", skiprows=1, unpack=True, encoding='UTF8')
 newx = 1000 * newx * 2 * np.pi
 newy = newy * np.sqrt(2)
-M = functions.mutual(0.05)
 
-
-def func(x, a, b, c, d, e):
-    s = a * x ** 4 - M ** 2 * x ** 4 - c * x ** 2 + e
-    t = d * x - b * x ** 3
-    p = M * x ** 2 * V0
-    A = p * t / (s ** 2 + t ** 2)
-    B = p * s / (s ** 2 + t ** 2)
-    return 1e10 * np.sqrt(A ** 2 + B ** 2)
-
-
-def funcvar(x, L1, L2, R1, R2, C1):
-    a = L1 * L2
-    b = L1 * R2 + R1 * L2
-    c = L1 / 1e-10 + R1 * R2 + L2 / C1
-    d = R1 / 1e-10 + R2 / C1
-    e = 1 / (C1 * 1e-10)
-    return func(x, a, b, c, d, e)
-
-
-def sanity(L1, L2, R1, R2, C1):
-    a = L1 * L2
-    b = L1 * R2 + R1 * L2
-    c = L1 / 1e-10 + R1 * R2 + L2 / C1
-    d = R1 / 1e-10 + R2 / C1
-    e = 1 / (C1 * 1e-10)
-    return [a, b, c, d, e]
-
-
-def pula(x, a, b, c, d, e, M):
-    s = a * x ** 4 - M ** 2 * x ** 4 - c * x ** 2 + e
-    t = d * x - b * x ** 3
-    p = M * x ** 2 * V0
-    A = p * t / (s ** 2 + t ** 2)
-    B = p * s / (s ** 2 + t ** 2)
-    return 1e10 * np.sqrt(A ** 2 + B ** 2)
-
-
-popt, pcov = curve_fit(func, xdata, ydata, p0=[0.01, 1.4, 2e9, 1.4e14, 1e20], maxfev=500000)
-print(M)
+popt, pcov = curve_fit(functions.volt, xdata, ydata, p0=[0.01, 1.4, 2e9, 1.4e14, 1e20], maxfev=500000)
+print(functions.M)
 
 print(popt)
 print(functions.circuit_values(*popt))
-print(sanity(*functions.circuit_values(*popt)))
-plt.plot(xdata, func(xdata, *popt), label='Fit')
-plt.plot(xdata, ydata, label='Data')
-plt.plot(xdata, funcvar(xdata, *functions.circuit_values(*popt)), label='Fit but with extracted RLC values')
+print(functions.letters(*functions.circuit_values(*popt)))
+plt.plot(xdata, functions.volt(xdata, *popt), label='Fit')
+plt.plot(xdata, ydata, label='DZata')
+plt.plot(xdata, functions.voltvar(xdata, *functions.circuit_values(*popt)), label='Fit but with extracted RLC values')
 plt.xlabel('ω [rad/s]')
 plt.ylabel('Peak voltage [V]')
 plt.legend()
 plt.show()
 
-# plt.plot(newx, newy)
-# plt.plot(newx, func(newx, 0.01, 1.4, 2e9, 1.4e11, 1e20), label='fit')
-# plt.legend()
-# plt.show()
+plt.plot(newx, newy)
+plt.plot(newx, functions.voltdis(newx, *popt, functions.mutual(0.35)), label='fit')
+plt.legend()
+plt.show()
 
-p, = plt.plot(xdata, func(xdata, *popt))
-plt.ylim(0,100)
+p, = plt.plot(xdata, functions.volt(xdata, *popt))
+plt.ylim(0, 100)
 axslider = plt.axes([0.1, 0, 0.8, 0.05])
 slider = Slider(ax=axslider, valmin=5, valmax=35, valinit=5, label='Distance (cm)')
 
 
 def value_update(val):
     M = functions.mutual(slider.val / 100)
-    p.set_ydata(pula(xdata, *popt, M))
+    p.set_ydata(functions.voltdis(xdata, *popt, M))
 
 
 slider.on_changed(value_update)
+plt.show()
+
+xdis, ydis = np.loadtxt("vdis.csv", delimiter=",", skiprows=1, unpack=True, encoding='UTF8')
+ydis = ydis * np.sqrt(2)
+
+
+def vdis(x, a, b, c, d, e):
+    xfreq = np.linspace(0, 1000000, 10000)
+    y = np.zeros(len(x))
+    for i in range(len(x)):
+        y[i] = max(functions.voltdis(xfreq, a, b, c, d, e, functions.mutual(x[i] / 100)))
+    return y
+
+
+popt1, pcov1 = curve_fit(vdis, xdis, ydis, p0=[*functions.letters(0.1, 0.1, 60, 7, 1e-10)], maxfev=500000)
+print(functions.circuit_values(*popt1))
+plt.plot(xdis, ydis)
+plt.plot(xdis, vdis(xdis, *popt1), label='fit')
+
+plt.legend()
 plt.show()
